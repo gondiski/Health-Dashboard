9d3b3dc14513d5aa3a7baf4629df8d17ba5da2e1
gondiski <enochotieno@rocketmail.com>
2020-11-15 09:35:30 +0300

    Added routes for various pages

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import Dashboard from './components/layouts/Dashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import './App.css';
-import Header from './components/sections/Header'
+import Header from './components/sections/Header';
+import Overview from './components/layouts/Overview';
+import Reports from './components/layouts/Reports';
+import Settings from './components/layouts/Settings';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 function App() {
-    return ( <
-        div className = "App bg-dark" >
+    return ( 
+
+        <div className = "App" >
+        <React.Fragment>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Dashboard} />
+                    <Route exact path="/overview" component={Overview} />
+                    <Route exact path="/reports" component={Reports} />
+                    <Route exact path="/settings" component={Settings} />
+                </Switch>
+            </Router>
+        </React.Fragment>
         <Container fluid style={{ height: '100%' }} className="bg-dark">
         <Header/>
         <Dashboard/>
         </Container>
-        < /
-        div >
+        </div >
     );
 }
 
--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -14,9 +14,9 @@ class Header extends Component {
             Navbar.Brand href = "#home" > My Dashboard < /Navbar.Brand> <
             Nav className = "mr-auto" >
             <
-            Nav.Link href = "#overview" > Overview < /Nav.Link> <
-            Nav.Link href = "#reports" > Reports < /Nav.Link> <
-            Nav.Link href = "#settings" > Settings < /Nav.Link> < /
+            Nav.Link href = "/overview" > Overview < /Nav.Link> <
+            Nav.Link href = "/reports" > Reports < /Nav.Link> <
+            Nav.Link href = "/settings" > Settings < /Nav.Link> < /
             Nav > <
             /Navbar>    < /
             Container >
