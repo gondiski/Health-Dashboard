52b1c19bd90e156b6bfc3158131df95779e93fb1
gondiski <enochotieno@rocketmail.com>
2020-11-19 23:18:24 +0300

    Added the changes to the buttons for sign up and sign in

--- src/components/layouts/Landing.js
+++ src/components/layouts/Landing.js
@@ -10,7 +10,7 @@ function Landing() {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     return ( <
-        Row >
+        Row className = "mt-5" >
         <
         Col className = "bg-dark shadow-lg mx-4" >
         <
@@ -19,7 +19,7 @@ function Landing() {
         Col md = "6"
         lg = "8" >
         <
-        h1 className = "lead display-3 text-white" > Take charge of your health. < /h1> <
+        h1 className = "font-weight-light display-3 text-white" > Take charge of your health. < /h1> <
         p className = "text-white" > Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. < /p> <
         Button className = "btn btn-light rounded mb-3" > More < /Button>  <
         Button className = "btn btn-light mb-3"
@@ -126,7 +126,7 @@ function Landing() {
         label = "Check me out" / >
         <
         /Form.Group> <
-        Button variant = "primary"
+        Button variant = "dark"
         type = "submit" >
         Submit <
         /Button> < /
@@ -138,7 +138,7 @@ function Landing() {
         onClick = { handleClose } >
         Close <
         /Button> <
-        Button variant = "primary"
+        Button variant = "dark"
         onClick = { handleClose } >
         Login <
         /Button> < /
