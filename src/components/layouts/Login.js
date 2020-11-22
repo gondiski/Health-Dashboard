3e93be5914b022b97624ea8c59e9c533e7de3c85
gondiski <enochotieno@rocketmail.com>
2020-11-22 07:42:27 +0300

    Added redux and redux thunk together with login and sign up js files

---
+++ src/components/layouts/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+class Login extends Component {
+    render() {
+        return ();
+    }
+}
+
+export default Login;
